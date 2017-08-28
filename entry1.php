250e7ee11702b6eca6d1eced3087e392384ff528
Work_desktop <creator83@yandex.ru>
2017-08-28 17:42:09 +0300

    s

---
+++ entry1.php
@@ -0,0 +1,24 @@
+
+<!DOCTYPE html>
+
+<head>
+	<title>Вход</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<link rel="stylesheet" href="entry.css" />
+
+</head>
+<body>
+	<section>
+	<form action="">
+		
+		<label>Имя пользователя: </label>
+		<input type="text" name="login" value=""/><br/ >
+		<label>Пароль: </label>
+		<input type="password" name="pass"/><br/>
+		<input type="submit" value="Подключится"/>
+		<input type="reset" value="очистить"/>
+		</div>
+	</form>
+	</section>
+</body>
+</html>
