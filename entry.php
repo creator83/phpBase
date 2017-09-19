6d17e572ba2c1a55c354c23fe2bb291e9245828d
laptop <creator83@yandex.ru>
2017-09-19 08:01:39 +0300

    's'

--- entry.php
+++ entry.php
@@ -18,3 +18,30 @@
 	</form>
 </body>
 </html>
+<?php
+class User{
+	public $name;
+	public $login;
+	public $password;
+
+	function __construct ($n, $l, $p){
+		$this->name = $n;
+		$this->login = $l;
+		$this->password = $p;
+
+	}
+	function showInfo (){
+		echo "<p>Name:".$this->name."</p><br>";
+		echo "<p>Login:".$this->login."</p><br>";
+		echo "<p>Password:".$this->password."</p>";
+		
+	}
+}
+$user1 = new User ("John Smith", "john", "1234");
+$user1->showInfo();
+$user2 = new User ("Mike Doe", "mike", "5678");
+$user2->showInfo();
+$user3 = new User ("Fedya Pupkin", "fedya", "7890");
+$user3->showInfo();
+
+?>
